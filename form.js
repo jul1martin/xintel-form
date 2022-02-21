4624c8638cdf2bce2bcfa0d19bf000e4dbe97925
Julian Martin <julianmartiniscoff@gmail.com>
2022-02-21 11:51:19 -0300

    Legibilidad del codigo

--- form.js
+++ form.js
@@ -1,144 +1,91 @@
-var boton = document.getElementById('newUser');
-var inputsArray,inputs,selectsArray,selects;
-var numSelectsName = 0;
-var currentTab = 2;
-
-var modalSave = new bootstrap.Modal(document.getElementById('modalSave'), {
-})
-// var lastPage = document.getElementById("lastPage");
-
-
-boton.addEventListener('click', newUser);
-function newUser(e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    var env = document.getElementById('send');
-    var send = document.createElement("div");
-    numSelectsName++;
-
-    send.innerHTML =
-    `<div class="divisor"></div>
-    <div class="mb-2 input-form form-block">
-    <input type="text" name="nom_user`+numSelectsName+`" autocomplete="name" required>
-    <label class="label-input" alt="Label" data-placeholder="Nombre completo"></label>
-    </div>
-    <div class="mb-2 input-form form-block">
-    <input type="text" name="email_personal_user`+numSelectsName+`" autocomplete="email" required>
-    <label class="label-input" alt="Label" data-placeholder="Correo electronico personal"></label>
-    </div>
-    <div class="mb-2 input-form form-block">
-    <input type="number" name="dni_user`+numSelectsName+`" autocomplete="off" required>
-    <label class="label-input" alt="Label" data-placeholder="Documento Nacional de Identidad"></label>
-    </div>`;
-    env.appendChild(send);
-    return showTab(currentTab);
-}
-
 var tab = document.getElementsByClassName("tab");
+var bntNewUser = document.getElementById('newUser');
+var prevBtn = document.getElementById("prevBtn");
+var nextBtn = document.getElementById("nextBtn");
 var validate = false;
 var advState = false;
+var currentTab = 0;
+var inputsArray,inputs,selectsArray,selects, validInput, validSelect;
+var modalSave = new bootstrap.Modal(document.getElementById('modalSave'), {})
+// var lastPage = document.getElementById("lastPage");
+nextBtn.addEventListener ("click",function(){nextPrev(1)})
+prevBtn.addEventListener ("click",function(){nextPrev(-1)})
 
 showTab(currentTab);
 
-function showTab (n) {
+function showTab(n) {
     tab[n].style.display= "block";
+    prevBtn.style.display = "inline";
+    nextBtn.innerHTML = "Siguiente";
 
-    if (n == 0) {
-        document.getElementById("prevBtn").style.display = "none";
-    } else {
-        document.getElementById("prevBtn").style.display= "inline";
-    }
+    if (n == 0) prevBtn.style.display = "none";
+    if (n == (tab.length - 1)) nextBtn.innerHTML = "Cargar empresa";
 
-    if (n == (tab.length - 1)) {
-        var saveCompany = document.getElementById("nextBtn");
-        saveCompany.innerHTML = "Cargar empresa";
-    } else {
-        document.getElementById("nextBtn").innerHTML = "Siguiente";
-    }
-
-    var validInput;
-    var validSelect;
-    var step = document.getElementsByClassName("step")[currentTab];
     selects = tab[currentTab].getElementsByTagName("select");
-    inputs = tab[currentTab].getElementsByTagName("input");
     selectsArray = Array.from(selects);
+    inputs = tab[currentTab].getElementsByTagName("input");
     inputsArray = Array.from(inputs);
+
     if(!selectsArray.length) validSelect = true;
-    if(!inputsArray.length) inputsArray = true;
-    inputsArray.forEach(input => {
-        input.addEventListener("keyup",input => {
-            validInput = true;
-            if (validate) {
-                if (!input.target.value) {
-                    input.target.style.backgroundColor = "#ffdddd";
-                } else {
-                    input.target.style.backgroundColor = "white";
-                    var test = input.target.getAttribute("autocomplete","email")
-                    console.log(test)
-                
-                }
 
-                for (i = 0; i < inputs.length; i++) {
-                    if (!inputs[i].value) validInput = false; 
-                }
-                // if (validInput) {
-                //     home.style.color = "black"; 
-                //     adv.style.display = "none";
-                //     step.className += " finish";
-                //     advState = false;
-                // }
-                if (validInput && validSelect) {
-                    home.style.color = "black"; 
-                    adv.style.display = "none";
-                    // step.className += " finish";
-                    step.style.backgroundColor = "#04AA6D";
-
-                    advState = false;
-                }
-            }
-        });
-    });
-    selectsArray.forEach(select => {
-        select.addEventListener("click",select => {
-            validSelect = true;
-
-            if (validate) {
-                if (!select.target.value) {
-                    select.target.style.backgroundColor = "#ffdddd";
-                } else {
-                    select.target.style.backgroundColor = "white";
-                }
-        
-                for (i = 0; i < selects.length; i++) {
-                    if (!selects[i].value) validSelect = false; 
-                }
-                // if (validSelect) {
-                //     home.style.color = "black"; 
-                //     adv.style.display = "none";
-                //     step.className += " finish";
-                //     advState = false;
-                // }
-                if (validInput && validSelect) {
-                    home.style.color = "black"; 
-                    adv.style.display = "none";
-                    // step.className += " finish";
-                    step.style.backgroundColor = "#04AA6D";
-                    advState = false;
-                }
-            }
-
-        });
-    });
+    inputsArray.forEach(input => { input.addEventListener("keyup",inputValidate) });
+    selectsArray.forEach(select => { select.addEventListener("click",selectValidate) });
 
     stepIndicator(n);
+}
 
+function inputValidate(input) {
+    var step = document.getElementsByClassName("step")[currentTab];
+
+    if(!inputsArray.length) inputsArray = true;
+
+    validInput = true;
+    if (validate) {
+        input.target.style.backgroundColor = "white";
+        if (!input.target.value) input.target.style.backgroundColor = "#ffdddd";
+
+        for (i = 0; i < inputs.length; i++) {
+            if (!inputs[i].value) validInput = false; 
+        }
+
+        if (validInput && validSelect) {
+            home.style.color = "black"; 
+            adv.style.display = "none";
+            // step.className += " finish";
+            step.style.backgroundColor = "#04AA6D";
+
+            advState = false;
+        }
+    }
+}
+
+function selectValidate(select) {
+    var step = document.getElementsByClassName("step")[currentTab];
+    validSelect = true;
+
+    if (validate) {
+        if (!select.target.value) {
+            select.target.style.backgroundColor = "#ffdddd";
+        } else {
+            select.target.style.backgroundColor = "white";
+        }
+
+        for (i = 0; i < selects.length; i++) {
+            if (!selects[i].value) validSelect = false; 
+        }
+
+        if (validInput && validSelect) {
+            home.style.color = "black"; 
+            adv.style.display = "none";
+            // step.className += " finish";
+            step.style.backgroundColor = "#04AA6D";
+            advState = false;
+        }
+    }
 }
 
 function nextPrev(n) {
-    var tab = document.getElementsByClassName("tab");
-
     if (n == 1 && !formValidate()) return false;
-
 
     tab[currentTab].style.display = "none";
     currentTab = currentTab + n;
@@ -147,20 +94,18 @@ function nextPrev(n) {
     showTab(currentTab);
 }
 
-function formValidate () {
-    var tab, inputs,selects, i, valid = true;
+function formValidate() {
+    var i, isValid = true;
     var adv = document.getElementById("adv");
     var home = document.getElementById("home");
 
-    tab = document.getElementsByClassName("tab");
     inputs = tab[currentTab].getElementsByTagName("input");
     selects = tab[currentTab].getElementsByTagName("select");
 
     for (i = 0; i < inputs.length; i++) {
         if (!inputs[i].value) {
             inputs[i].style.backgroundColor = "#ffdddd";
-
-            valid = false;
+            isValid = false;
             validate = true;
             if (advState == false) {
                 adv.style.display = "block";
@@ -178,8 +123,7 @@ function formValidate () {
     for (i = 0; i < selects.length; i++) {
         if (!selects[i].value) {
             selects[i].style.backgroundColor = "#ffdddd";
-
-            valid = false;
+            isValid = false;
             validate = true;
             if (advState == false) {
                 adv.style.display = "block";
@@ -194,34 +138,56 @@ function formValidate () {
             } 
     }
 
-    if (valid) {
-        // document.getElementsByClassName("step")[currentTab].className += " finish";
-        document.getElementsByClassName("step")[currentTab].style.backgroundColor = "#04AA6D";
-    } else {
-        // document.getElementsByClassName("step")[currentTab].className += " incomplete";
-        document.getElementsByClassName("step")[currentTab].style.backgroundColor = "red";
+    if (isValid) {
+        document.getElementsByClassName("step")[currentTab].style.backgroundColor = "#04AA6D"; // document.getElementsByClassName("step")[currentTab].className += " finish";
+        return isValid;
     }
+        document.getElementsByClassName("step")[currentTab].style.backgroundColor = "red"; // document.getElementsByClassName("step")[currentTab].className += " incomplete";
+        return isValid;
+}
 
-    return valid;
+bntNewUser.addEventListener('click', newUser);
+
+function newUser(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    
+    var numSelectsName = 0;
+    var send = document.getElementById('send');
+    var divSend = document.createElement("div");
+    numSelectsName++;
+
+    divSend.innerHTML =
+    `<div class="divisor"></div>
+    <div class="mb-2 input-form form-block">
+    <input type="text" name="nom_user`+numSelectsName+`" autocomplete="name" required>
+    <label class="label-input" alt="Label" data-placeholder="Nombre completo"></label>
+    </div>
+    <div class="mb-2 input-form form-block">
+    <input type="text" name="email_personal_user`+numSelectsName+`" autocomplete="email" required>
+    <label class="label-input" alt="Label" data-placeholder="Correo electronico personal"></label>
+    </div>
+    <div class="mb-2 input-form form-block">
+    <input type="number" name="dni_user`+numSelectsName+`" autocomplete="off" required>
+    <label class="label-input" alt="Label" data-placeholder="Documento Nacional de Identidad"></label>
+    </div>`;
+    send.appendChild(divSend);
+
+    return showTab(currentTab);
 }
 
 function stepIndicator(n) {
     var i, step = document.getElementsByClassName("step");
-    for (i = 0; i < step.length; i++) {
-        step[i].className = step[i].className.replace(" active", " ")
-    }
-
+    for (i = 0; i < step.length; i++) step[i].className = step[i].className.replace(" active", " ");
+    
     step[n].className += " active";
 }
 
-var dinamicSelect = document.getElementById("dinamicSelect")
-var dinamicHidden = document.getElementById("dinamicHidden")
+var dinamicSelect = document.getElementById("dinamicSelect");
+var dinamicHidden = document.getElementById("dinamicHidden");
+
 dinamicSelect.addEventListener("click", function(){
-    if (dinamicSelect.value == "integracion") {
-        return dinamicHidden.removeAttribute("hidden") 
-    }
-    if (!dinamicHidden.getAttribute("hidden")) {
-        dinamicHidden.setAttribute("hidden","") 
-    }
+    if (dinamicSelect.value == "integracion") return dinamicHidden.removeAttribute("hidden") 
+    if (!dinamicHidden.getAttribute("hidden")) return dinamicHidden.setAttribute("hidden","")
     
 })
